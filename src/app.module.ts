6c246c211355a1a3170d83b7a1a3116f8bdc7641
Tim Hrovat <tim.hrovat16@gmail.com>
2022-12-18 10:29:51 +0100

    changed imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { PrismaModule } from './prisma/prisma.module';
 import { StudentsModule } from './students/students.module';
 import { ClassesModule } from './classes/classes.module';
 import { EmployeesModule } from './employees/employees.module';
-import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   imports: [PrismaModule, StudentsModule, ClassesModule, EmployeesModule],
